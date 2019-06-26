f25db17aace2aa5cb3ad0be4d769fed9492ee7b9
Kicho94 <49168212+Kicho94@users.noreply.github.com>
2019-06-26 18:57:08 +0200

    domasni

---
+++ Domasni/d2.js
@@ -0,0 +1,34 @@
+var fs = require('fs');
+var tekst = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin at urna lorem. Mauris et posuere metus. Sed luctus posuere orci, a dictum ligula venenatis sed. Sed sed turpis a nisl porttitor gravida. Sed fermentum laoreet felis. Sed id fermentum justo. Etiam egestas est urna, ut pulvinar est hendrerit eu. Sed quis convallis purus. Donec pellentesque arcu vel nunc molestie posuere. Vestibulum a sapien ex. Vivamus pellentesque est vitae egestas imperdiet. Morbi dictum vestibulum arcu, sit amet eleifend lectus consequat commodo. Proin egestas augue nec fermentum ultrices. Donec finibus lobortis augue vitae scelerisque.
+
+Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Suspendisse aliquam augue eu nisi interdum, in posuere tellus fermentum. Cras eu velit augue. Vivamus enim velit, maximus vel purus id, iaculis malesuada eros. Morbi egestas erat nec sapien convallis, quis ullamcorper eros imperdiet. Aenean venenatis est velit, ultricies sollicitudin augue imperdiet tincidunt. Proin quis eleifend sapien. Proin ut molestie massa, a rhoncus magna. Quisque feugiat quam commodo lectus consequat, eu aliquet dolor auctor.`
+fs.writeFile("domasna.txt", tekst, (err) =>{
+    if (err){
+        console.error(err)
+    }
+} );
+
+
+fs.readFile("domasna.txt","utf8",(err,data)=>{
+    if(err){
+        console.log(err)
+    }
+    totalCharCount(data);
+    totalWordCount(data);
+    totalSentCount(data);
+
+    
+
+});
+
+var totalCharCount = (data) => {
+    console.log("There are " + data.length + " characters,signs and empty spaces in this text.");
+};
+
+var totalWordCount = (data) => {
+   console.log("There are " + data.split(" ").length + " words in this text")
+   console.log("There are "+ data.spli)
+};
+
+
+
